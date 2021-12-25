29d317efbcdf04ce233ae4cc05a163348e072539
vedansh <Vedansh@quadrical.ai>
2021-12-26 01:15:42 +0530

    Exploring Modules

---
+++ Lecture 1/modules.js
@@ -0,0 +1,12 @@
+// CommonJS, every file is a moduel
+// Modules - Encapsulated Code (only share minimum)
+
+const john = 'john'
+const peter = 'peter'
+
+const sayHi = (name) => {
+  console.log(`Hello There ${name} `);
+};
+
+sayHi(john);
+sayHi(peter)
