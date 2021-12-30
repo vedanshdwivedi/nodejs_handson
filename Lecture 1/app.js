3d046bd5418ba716bd4fe5c2ac7a6e39a4a248e4
vedansh <Vedansh@quadrical.ai>
2021-12-31 02:23:45 +0530

    Utilising NPM

--- Lecture 1/app.js
+++ Lecture 1/app.js
@@ -1,7 +1,11 @@
 const _ = require("lodash");
 
-const items = [1, [2, [3, [4]]]];
+const items = [1, [2, [3, [4, [5]]]]];
 
 const new_items = _.flattenDeep(items);
 
 console.log(new_items);
+
+console.log("Hello World");
+
+console.log("Turned off Auto Save");
